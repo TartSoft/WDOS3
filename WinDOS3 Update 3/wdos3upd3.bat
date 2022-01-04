4c69f734b18d9c6d0693890fd9752fdd2fbe6b0b
Le Trieu Duong <92847004+duongletrieu@users.noreply.github.com>
2022-01-04 19:35:22 +0700

    Adding explanations to solution in WinDOS3
    Adding explanations in rem (with links)

--- WinDOS3 Update 3/wdos3upd3.bat
+++ WinDOS3 Update 3/wdos3upd3.bat
@@ -15,6 +15,9 @@
 color 02
 title WinDOS3
 rem ASCII arts :)
+rem I need to add "^" characters here before "|" to 
+rem run the art. You can find more at:
+rem https://stackoverflow.com/questions/11356787/how-can-i-insert-this-ascii-text-art-in-cmd-or-command-prompt/11434695#11434695
 echo -----------------------------------------------
 echo  _    _ _      ______ _____ _____ 
 echo ^| ^|  ^| (_)     ^|  _  \  _  /  ___^|
